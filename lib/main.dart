5d0b59c82917d26afb3a1c672e2b1224bdc96482
esebeto_ <albertoalonso@outlook.com>
2023-09-14 22:20:46 -0600

    Cambio de Nombre de La App

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Basic Demo',
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
